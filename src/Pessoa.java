22f87f09fd89d3e0dec54486e422dd0ae1aabde4
Leonardo Rocha <leonardorochasantos9@gmail.com>
2023-06-15 11:51:59 -0300

    Initial commit

---
+++ src/Pessoa.java
@@ -0,0 +1,16 @@
+public class Pessoa {
+
+    //Atributos
+    String nome;
+    double cpf;
+    double rg;
+    int telefone;
+
+
+
+    //Métodos
+    public void pesquisarEmpresa(){
+
+    }
+
+}
