bb5122f24a074ea8b7a292d0461ff8a69509289c
Leonardo Rocha <leonardorochasantos9@gmail.com>
2023-08-10 11:24:21 -0300

    Pessoa, Piloto e Balao

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,20 @@ public class Main {
     public static void main(String[] args) {
         //Criar objetos do passeio
         Pessoa p1 = new Pessoa();
-        Pessoa p2;
-        p2 = new Pessoa();
+        Pessoa p2 = new Pessoa();
+
         //...
         p1.nome = "Elliot Page";
         p1.CPF = "10203444173";
         p1.RG = "121572501";
         p1.telefone = "123443211";
         p1.telemergencia = "212122121";
+
+        p2.nome = "Antonella Bascete";
+        p2.CPF = "42536589120";
+        p2.RG = "517206835";
+        p2.telefone = "912044193";
+        p2.telemergencia = "218811889";
+
     }
 }
